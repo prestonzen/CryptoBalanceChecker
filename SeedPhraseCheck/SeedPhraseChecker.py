6b776df4c603855255cc06df5acf99a5d2ce8324
Preston Zen <prestonzen@kaizenapps.com>
2023-10-11 19:50:26 +0300

    Updated for only BIP 39 and 44

--- SeedPhraseCheck/SeedPhraseChecker.py
+++ SeedPhraseCheck/SeedPhraseChecker.py
@@ -1,15 +1,14 @@
 import json
 import csv
-import asyncio
-from web3 import Web3, HTTPProvider
+from web3 import Web3
 from mnemonic import Mnemonic
 from eth_account import Account
 from bip_utils import Bip44, Bip39SeedGenerator, Bip44Coins, Bip44Changes
 
-# Initialize a Web3 instance with async provider
-w3 = Web3(HTTPProvider('https://mainnet.infura.io/v3/70a8ace7afb54c7fa0de6504c36be15b', {"timeout": 60}))
+# Initialize a Web3 instance
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/70a8ace7afb54c7fa0de6504c36be15b'))
 
-app_version = 7  # Incremented from previous version
+app_version = 10  # Incremented from previous version
 
 def derive_bip44_private_key(seed_bytes):
     """Derive private key using BIP-44 standard."""
@@ -24,30 +23,39 @@ def derive_bip39_private_key(seed_phrase):
     seed_bytes = Bip39SeedGenerator(seed_phrase).Generate()
     return derive_bip44_private_key(seed_bytes)
 
-async def check_transaction_activity(address):
-    """Asynchronously check transaction activity for a given Ethereum address."""
-    tx_count = await asyncio.to_thread(w3.eth.getTransactionCount, address)
+def check_transaction_activity(address):
+    """Check transaction activity for a given Ethereum address."""
+    tx_count = w3.eth.getTransactionCount(address)
     return tx_count > 0
 
 def import_txt(filepath):
     """Import seed phrases from a .txt file."""
-    with open(filepath, 'r') as file:
-        return [line.strip() for line in file]
+    try:
+        with open(filepath, 'r') as file:
+            return [line.strip() for line in file]
+    except FileNotFoundError:
+        return []
 
 def import_csv(filepath):
     """Import seed phrases from a .csv file."""
-    with open(filepath, 'r') as file:
-        reader = csv.reader(file)
-        return [",".join(row).replace(",", " ").strip() for row in reader]
+    try:
+        with open(filepath, 'r') as file:
+            reader = csv.reader(file)
+            return [",".join(row).replace(",", " ").strip() for row in reader]
+    except FileNotFoundError:
+        return []
 
 def import_json(filepath):
     """Import seed phrases from a .json file."""
-    with open(filepath, 'r') as file:
-        data = json.load(file)
-        return list(data.values())
+    try:
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+            return list(data.values())
+    except FileNotFoundError:
+        return []
 
-async def process_seed(seed):
-    """Asynchronously process a seed phrase to derive private keys using multiple BIP standards, fetch associated addresses and balances, and check transaction activity."""
+def process_seed(seed):
+    """Process a seed phrase to derive private keys using multiple BIP standards, fetch associated addresses and balances, and check transaction activity."""
     seed_bytes = Mnemonic("english").ToSeed(seed)
     
     for method, bip_func in [("BIP-39", derive_bip39_private_key), 
@@ -57,7 +65,7 @@ async def process_seed(seed):
         address = account.address
 
         # If no transaction activity, skip further processing for this BIP type
-        has_activity = await check_transaction_activity(address)
+        has_activity = check_transaction_activity(address)
         if not has_activity:
             continue
 
@@ -72,7 +80,8 @@ seed_phrases.extend(import_txt("seedPhrases.txt"))
 seed_phrases.extend(import_csv("seedPhrases.csv"))
 seed_phrases.extend(import_json("seedPhrases.json"))
 
-# Running the asynchronous processing
-loop = asyncio.get_event_loop()
-tasks = [process_seed(seed) for seed in seed_phrases]
-loop.run_until_complete(asyncio.gather(*tasks))
+if not seed_phrases:
+    print("No valid file detected.")
+else:
+    for seed in seed_phrases:
+        process_seed(seed)
